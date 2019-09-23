c78f532c2f3ea931f00ec330ae039b6e3c18b936
awjh <andrewhurt@btinternet.com>
2019-09-23 20:11:27 +0100

    fix cli when in .bin
    Signed-off-by: awjh <andrewhurt@btinternet.com>

--- src/cli.ts
+++ src/cli.ts
@@ -1,16 +1,24 @@
 #!/usr/bin/env node
 
+import * as fs from 'fs-extra';
+import * as path from 'path';
 import * as yargs from 'yargs';
-import packageJSON from '../package.json';
 
 /* istanbul ignore file */
 
+let modulePath =  path.join(__dirname, '..');
+
+if (path.basename(__dirname) === '.bin') {
+    modulePath = path.join(__dirname, '../license-check-and-add');
+}
+
+const packageJSON = fs.readJSONSync(path.join(modulePath, '../package.json'));
 const version = 'v' + packageJSON.version;
 
 try {
     // tslint:disable-next-line: no-unused-expression
     yargs
-    .commandDir('./cmds', {exclude: /.*\.spec\.js/})
+    .commandDir(path.join(modulePath, 'src/cmds'), {exclude: /.*\.spec\.js/})
     .demandCommand(1, 'Need to specify a command')
     .help()
     .wrap(null)
