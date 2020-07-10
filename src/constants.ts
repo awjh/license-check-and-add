c9db605a1aeac752206d7d2be1f90a9d0bc45b43
awjh <andrewhurt@btinternet.com>
2020-07-10 15:15:19 +0100

    impl no test
    Signed-off-by: awjh <andrewhurt@btinternet.com>

--- src/constants.ts
+++ src/constants.ts
@@ -15,6 +15,11 @@ export const OPTIONS: {[s: string]: Options} = {
         required: false,
         type: 'string',
     },
+    'regex-replacement': {
+        alias: ['r'],
+        required: false,
+        type: 'string',
+    },
 };
 
 export const CONFIG_OPTION = 'config-file';
